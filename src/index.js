66f01c88dd013cc059c4a1945764060157d41f4b
“Shawna <shawna.e.hartley@gmail.com>
2023-01-14 20:06:16 -0500

    choropleth complete

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-// import map from './scripts/map.js';
+import Choropleth from './scripts/chroropleth' 
+import hale from './scripts/hale.json'
+import world from './scripts/world.json'
+import countrymesh from './scripts/countrymesh.json'
 
+const countries = topojson.feature(world, world.objects.countries);
+const chart = Choropleth(hale, {
+  id: d => d.name, // country name, e.g. Zimbabwe
+  value: d => d.hale, // health-adjusted life expectancy
+  range: d3.interpolateYlGnBu,
+  features: countries,
+  featureId: d => d.properties.name, // i.e., not ISO 3166-1 numeric
+  borders: countrymesh,
+  projection: d3.geoEqualEarth(),
+  title: (_, d) => d ? d.name : '',
+})
 
-// import {Histogram} from "d3/histogram"
-const values = [1.21, 1.02, 0.61, 0.99, 1.53]
+document.body.appendChild(chart);
 
-function main() {
-  d3.select("p").style("color", "firebrick");
-}
-
-// console.log(fetch('https://population.un.org/dataportalapi/api/v1'))
-// will add an object as the second argument for the selector we want(this is all indicators)
-// returns a promise, therefore can use async await or .then or .catch
-  // .then(res => console.log(res))
-  // returning an error "cannot read properties of undefined (reading 'then')"
-
-  d3.select
-
+console.log(typeof hale)
 
 // document.addEventListener('DOMContentLoaded', () => {
 //   // console.log("hello world")
