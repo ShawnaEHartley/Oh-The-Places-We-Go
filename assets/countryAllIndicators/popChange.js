c4ef3d8acc6bd168cb7a70ce45f6be04bd535746
Shawna Hartley <116672563+ShawnaEHartley@users.noreply.github.com>
2023-08-07 17:08:59 -0400

    added portfolio page link and icon

--- assets/countryAllIndicators/popChange.js
+++ assets/countryAllIndicators/popChange.js
@@ -9,8 +9,7 @@ const popchangelist = [];
 async function main() {
   for (let i = 0; i < 284; i++) {
     const loc = locs.data[i];
-
-    console.log(`processing ${loc.name}`);
+    
     const totpop = await fetch (`https://population.un.org/dataportalapi/api/v1/data/indicators/49/locations/${loc.id}/start/2021/end/2021`);
     const dta = await totpop.json();
     if (!dta.data || !dta.data.length) { continue }
@@ -18,11 +17,8 @@ async function main() {
   
     const popchg = await fetch (`https://population.un.org/dataportalapi/api/v1/data/indicators/65/locations/${loc.id}/start/2021/end/2021`);
     const dta2 = await popchg.json();
-    // console.log(dta2);
-    // console.log(dta2.data[0])
     if (!dta2.data || !dta2.data.length) { continue }
     const popchange = dta2.data[0].value;
-    console.log(popchange);
   
     const percpopchg = (popchange/totalpop) * 100;
     popchangelist.push({name: loc.name, totalpop: totalpop, popchange: popchange, percpopchange: percpopchg})
